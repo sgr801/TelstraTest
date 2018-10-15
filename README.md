4250ef83020b498e3e02c05c5bbbf23d9e8b58b7
Shagar Shekh <sshekh@deputy.com>
2018-10-15 13:29:30 +1100

    Initial Commit Project Created

--- README.md
+++
@@ -1,3 +0,0 @@
-# TelstraTest
-
-Test
---
+++ app/src/main/java/com/shekh/test/telstra/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.shekh.test.telstra
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
