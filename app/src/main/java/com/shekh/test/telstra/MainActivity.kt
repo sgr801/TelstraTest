cddd2d27cbac2de985e85a43a097de41e5007747
Shagar Shekh <sshekh@deputy.com>
2018-10-19 16:28:36 +1100

    RecyclerView and Adapter added

--- app/src/main/java/com/shekh/test/telstra/MainActivity.kt
+++ app/src/main/java/com/shekh/test/telstra/MainActivity.kt
@@ -2,35 +2,60 @@ package com.shekh.test.telstra
 
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProviders
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import android.widget.Toast
 import com.shekh.test.telstra.helpers.Injector
 import com.shekh.test.telstra.model.PhotoResponse
+import com.shekh.test.telstra.ui.PhotosAdapter
 import com.shekh.test.telstra.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var photosAdapter: PhotosAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // get the view model
         viewModel = ViewModelProviders.of(this, Injector.provideViewModelFactory(this))
                 .get(MainViewModel::class.java)
-        initAdapter()
 
+        setupRecyclerView()
+        initAdapter()
+        swipeRefreshLayout.setOnRefreshListener {
+            viewModel.getPhotos()
+        }
+    }
+
+    private fun setupRecyclerView() {
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
     }
 
     private fun initAdapter() {
         viewModel.photos.observe(this, Observer<PhotoResponse> {
-            t1.text = it?.title
-            t2.text = it?.rows?.count()?.toString()
+            swipeRefreshLayout.isRefreshing = false
+            it?.title?.let { title ->
+                setActionBarTitle(title)
+            }
+
+            it?.rows?.let { rows ->
+                photosAdapter = PhotosAdapter(this, rows)
+                recyclerView.adapter = photosAdapter
+            }
         })
+
         viewModel.networkError.observe(this, Observer<String> {
+            swipeRefreshLayout.isRefreshing = false
             Toast.makeText(this, "Wooops $it", Toast.LENGTH_LONG).show()
         })
+
         viewModel.getPhotos()
+    }
+
+    private fun setActionBarTitle(title: String) {
+        supportActionBar?.title = title
     }
 }
---
+++ app/src/main/java/com/shekh/test/telstra/ui/PhotosAdapter.kt
@@ -0,0 +1,51 @@
+package com.shekh.test.telstra.ui
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.ProgressBar
+import android.widget.TextView
+import com.shekh.test.telstra.R
+import com.shekh.test.telstra.helpers.CustomViewUtils
+import com.shekh.test.telstra.model.Photo
+import kotlinx.android.synthetic.main.item_photo.view.*
+
+class PhotosAdapter(private val context: Context, private val mList: ArrayList<Photo>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoViewHolder {
+        return PhotoViewHolder(LayoutInflater.from(context).inflate(R.layout.item_photo, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+
+        val photo = mList[position]
+
+        if (holder is PhotoViewHolder) {
+            photo.title?.let {
+                holder.title.text = it
+            }
+
+            photo.description?.let {
+                holder.description.text = it
+            }
+
+            photo.imageHref?.let {
+                CustomViewUtils.showImageWithGlide(context, photo.imageHref, holder.photoProgressbar, holder.photo)
+            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    class PhotoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var title: TextView = itemView.titleTextView
+        var description: TextView = itemView.descriptionTextView
+        var photo: ImageView = itemView.photoImageView
+        var photoProgressbar: ProgressBar = itemView.photo_progress_bar
+    }
+}
