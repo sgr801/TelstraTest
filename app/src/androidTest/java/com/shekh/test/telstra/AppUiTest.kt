9df077cb3142d769532810ca5dd0dec49ca1a9ec
Shagar Shekh <sshekh@deputy.com>
2018-10-22 01:05:50 +1100

    UI test added

---
+++ app/src/androidTest/java/com/shekh/test/telstra/AppUiTest.kt
@@ -0,0 +1,39 @@
+package com.shekh.test.telstra
+
+import android.content.Context
+import androidx.test.core.app.ActivityScenario
+import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class AppUiTest {
+
+    /**
+     * Use [ActivityScenario] to create and launch the activity under test. This is a
+     * replacement for [androidx.test.rule.ActivityTestRule].
+     */
+    @Before
+    fun launchActivity() {
+        ActivityScenario.launch(MainActivity::class.java)
+    }
+
+    @Test
+    fun checkTextSuccess() {
+        val text = getApplicationContext<Context>().resources.getString(R.string.loading)
+        onView(withId(R.id.loadingTextView)).check(matches(withText(text)))
+    }
+
+    @Test
+    fun checkTextFailed() {
+        onView(withId(R.id.loadingTextView)).check(matches(withText("Loading")))
+    }
+}
